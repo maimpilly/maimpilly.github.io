b6607b546a8635205583a6e138e7b76ec2c196b3
Sarath <maimpilly@gmail.com>
2025-11-04 12:07:50 +0100

    Change in title

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sarath Chandran Maimpilly | Electrical Engineer';
-export const SITE_DESCRIPTION = 'The personal site and portfolio of Sarath Chandran Maimpilly, Electrical Engineer. Sharing my journey in tech from Germany. ';
+export const SITE_TITLE = 'Sarath Chandran Maimpilly';
+export const SITE_DESCRIPTION = 'The personal site and portfolio of Sarath Chandran Maimpilly. Sharing my journey in tech from Germany. ';
 
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
